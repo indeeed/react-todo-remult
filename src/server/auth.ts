b0e7a5844dbf56881dfa3b8ced05b03d88021d7b
like fools <likefools10@gmail.com>
2024-06-06 18:08:40 +0300

    start_app

---
+++ src/server/auth.ts
@@ -0,0 +1,33 @@
+// src/server/auth.ts
+
+import express, { Router } from "express"
+import type { UserInfo } from "remult"
+
+const validUsers: UserInfo[] = [
+  { id: "1", name: "admin", roles: ["admin"] },
+  { id: "2", name: "test2" },
+  { id: "3", name: "test" },
+  { id: "4", name: "israel", roles: ["admin"] },
+
+]
+
+export const auth = Router()
+
+auth.use(express.json())
+
+auth.post("/api/signIn", (req, res) => {
+  const user = validUsers.find((user) => user.name === req.body.username)
+  if (user) {
+    req.session!["user"] = user
+    res.json(user)
+  } else {
+    res.status(404).json("Invalid user, try 'Steve' or 'Jane'")
+  }
+})
+
+auth.post("/api/signOut", (req, res) => {
+  req.session!["user"] = null
+  res.json("signed out")
+})
+
+auth.get("/api/currentUser", (req, res) => res.json(req.session!["user"]))
---
+++ src/shared/Task.ts
@@ -0,0 +1,32 @@
+import { Allow, Entity, Fields } from "remult"
+
+@Entity("tasks", {
+  allowApiCrud: Allow.authenticated,
+  allowApiInsert: "admin",
+  allowApiDelete: "admin",
+})
+export class Task {
+  @Fields.cuid()
+  id = ""
+
+  @Fields.string()
+  userTask = ""
+
+
+  @Fields.string<Task>({
+    validate: (task) => {
+      if (task.title.length < 3) throw "Too Short"
+    },
+    allowApiUpdate: "admin",
+  })
+  title = ""
+
+  @Fields.string()
+  userCompleted = ""
+
+  @Fields.boolean()
+  completed = false
+
+  @Fields.createdAt()
+  createdAt = new Date
+}
