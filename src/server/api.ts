5db88b81b5a0a2899922d3924b30bcdd981f893e
like fools <likefools10@gmail.com>
2024-06-07 11:51:36 +0300

    config

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,15 +1,15 @@
 import { remultExpress } from "remult/remult-express"
 import { Task } from "../shared/Task"
 import { TasksController } from "../shared/TasksController"
-// import { createPostgresDataProvider } from "remult/postgres"
+import { createPostgresDataProvider } from "remult/postgres"
 
 export const api = remultExpress({
   entities: [Task],
   controllers: [TasksController],
   getUser: (req) => req.session!["user"],
-  // dataProvider: createPostgresDataProvider({
-  //   connectionString:
-  //     process.env["DATABASE_URL"] ||
-  //     "postgres://postgres:MASTERKEY@localhost/postgres",
-  // }),
+  dataProvider: createPostgresDataProvider({
+    connectionString:
+      process.env["DATABASE_URL"] ||
+      "postgres://postgres:1234@localhost/testing",
+  }),
 })
